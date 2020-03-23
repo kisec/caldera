25e2dc7e580caa7e921118de432c445776c822d9
javon <javon@mitre.org>
2020-03-23 11:24:32 -0400

    send contact update via udp

--- app/contacts/handles/h_beacon.py
+++ app/contacts/handles/h_beacon.py
@@ -11,8 +11,12 @@ class Handle:
         callback = message.pop('callback', None)
         message['executors'] = [e for e in message.get('executors', '').split(',') if e]
         message['contact'] = 'udp'
-        await services.get('contact_svc').handle_heartbeat(**message)
+        agent, _ = await services.get('contact_svc').handle_heartbeat(**message)
 
         if callback:
-            sock = socket.socket(family=socket.AF_INET, type=socket.SOCK_DGRAM)
-            sock.sendto('roger'.encode(), (caller, int(callback)))
+            if agent.contact != message['contact']:
+                sock = socket.socket(family=socket.AF_INET, type=socket.SOCK_DGRAM)
+                sock.sendto(('%s\t' % agent.contact).encode(), (caller, int(callback)))
+            else:
+                sock = socket.socket(family=socket.AF_INET, type=socket.SOCK_DGRAM)
+                sock.sendto('roger'.encode(), (caller, int(callback)))
